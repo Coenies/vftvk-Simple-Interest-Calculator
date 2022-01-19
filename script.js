70a621e2f25510338f19b98aaf0a0ce9cd1917c3
Coenie Smit <coenie.smit@gmail.com>
2022-01-20 00:31:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ function compute()
     var principal = document.getElementById("principal").value;
     if (!principal || principal < 1)
     {
-        alert("Enter a positive number")
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
         return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = parseFloat(principal) * parseFloat(years) * parseFloat(rate) / 100;
     var year = new Date().getFullYear() + parseInt(years);
-    var outtext = "If you deposit <em>" + principal + "</em>,</br>";
-    outtext += "at an interest rate of <em>" + rate + "</em>%.</br>";
-    outtext += "You will receive and amount of <em>" + interest + "</em>,</br>"
-    outtext += "in the year <em>" + year + "</em>";
+    var outtext = "If you deposit <span class='number'>" + principal + "</span>,</br>";
+    outtext += "at an interest rate of <span class='number'>" + rate + "</span>%.</br>";
+    outtext += "You will receive and amount of <span class='number'>" + interest + "</span>,</br>"
+    outtext += "in the year <span class='number'>" + year + "</span>";
     document.getElementById("result").innerHTML = outtext;
 }
       
